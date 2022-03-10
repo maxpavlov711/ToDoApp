e02afcf4f457f9ab3d360a0e596f15f42801ff76
Max Pavlov <max.pavlov711@gmail.com>
2022-03-10 18:57:05 +0300

    MockTaskCell

--- ToDoApp/View/TaskCell.swift
+++ ToDoApp/View/TaskCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class TaskCell: UITableViewCell {
 
-
+    func configure(withTask task: Task) {
+        
+    }
 
 }
--- ToDoAppTests/Controller/DataProviderTests.swift
+++ ToDoAppTests/Controller/DataProviderTests.swift
@@ -85,6 +85,18 @@ class DataProviderTests: XCTestCase {
         
         XCTAssertTrue(mockTableView.cellIsDequeude)
     }
+    
+    func testCellForRowInSectionZeroCallsConfigure() {
+        tableView.register(MockTaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
+        
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockTaskCell
+        
+        XCTAssertEqual(cell.task, task)
+    }
 }
 
 extension DataProviderTests {
@@ -95,6 +107,14 @@ extension DataProviderTests {
             cellIsDequeude = true
             
             return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        }
+    }
+    
+    class MockTaskCell: TaskCell {
+        var task: Task?
+        
+        override func configure(withTask task: Task) {
+            self.task = task
         }
     }
 }
