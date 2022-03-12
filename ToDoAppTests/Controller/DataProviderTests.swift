29551d204a4ff3efb051308d400ed006ca995250
Max Pavlov <max.pavlov711@gmail.com>
2022-03-12 13:12:54 +0300

    Check and Uncheck functionality

--- ToDoAppTests/Controller/DataProviderTests.swift
+++ ToDoAppTests/Controller/DataProviderTests.swift
@@ -123,6 +123,28 @@ class DataProviderTests: XCTestCase {
         
         XCTAssertEqual(buttonTitle, "Undone")
     }
+    
+    func testCheckingTaskChecksInTaskManager() {
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        
+        tableView.dataSource?.tableView?(tableView, commit: .delete, forRowAt: IndexPath(row: 0, section: 0))
+        
+        XCTAssertEqual(sut.taskManager?.tasksCount, 0)
+        XCTAssertEqual(sut.taskManager?.doneTasksCount, 1)
+    }
+    
+    func testUncheckingTaskUncheckInTaskManager() {
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        sut.taskManager?.checkTask(at: 0)
+        tableView.reloadData()
+        
+        tableView.dataSource?.tableView?(tableView, commit: .delete, forRowAt: IndexPath(row: 0, section: 1))
+        
+        XCTAssertEqual(sut.taskManager?.tasksCount, 1)
+        XCTAssertEqual(sut.taskManager?.doneTasksCount, 0)
+    }
 }
 
 extension DataProviderTests {
